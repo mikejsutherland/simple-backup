ec6226d1bf8db20406e45549c0f17de422a81aa8
Michael Sutherland <mike@codesmak.com>
2014-07-16 23:32:12 -0300

    Covert tab to spaces

--- simple-backup.sh
+++ simple-backup.sh
@@ -31,75 +31,75 @@ keep_archive=2;
 
 function rotate_archive() {
 
-	if [ "$#" -lt 2 ]; then
-		echo $"Usage: rotate_archive [target] [keep_archive]"
-		return 1
-	fi
+    if [ "$#" -lt 2 ]; then
+        echo $"Usage: rotate_archive [target] [keep_archive]"
+        return 1
+    fi
 
-	# Rotate archive disabled
-	[ $2 -lt 1 ] && return
+    # Rotate archive disabled
+    [ $2 -lt 1 ] && return
 
-	local target
+    local target
 
-	# Strip trailing slash
-	target=${1%*/}
-	# Strip base path, leaving target dir
-	target=${target##*/}
+    # Strip trailing slash
+    target=${1%*/}
+    # Strip base path, leaving target dir
+    target=${target##*/}
 
-	# Rotate archives
-	for ((i=$(($2-2)); i>=0; i--))
-	do
-		if [ -f "${save_dir}/${target}.${i}.tar.gz" ]
-		then
-			x=$((i+1));
-			echo "Rotating: ${save_dir}/${target}.${i}.tar.gz -> ${save_dir}/${target}.${x}.tar.gz"
-			mv "${save_dir}/${target}.${i}.tar.gz" "${save_dir}/${target}.${x}.tar.gz"
-		fi
-	done
+    # Rotate archives
+    for ((i=$(($2-2)); i>=0; i--))
+    do
+        if [ -f "${save_dir}/${target}.${i}.tar.gz" ]
+        then
+            x=$((i+1));
+            echo "Rotating: ${save_dir}/${target}.${i}.tar.gz -> ${save_dir}/${target}.${x}.tar.gz"
+            mv "${save_dir}/${target}.${i}.tar.gz" "${save_dir}/${target}.${x}.tar.gz"
+        fi
+    done
 
-	if [[ -f "${save_dir}/${target}.tar.gz" ]]
-	then
-		echo "Rotating: ${save_dir}/${target}.tar.gz -> ${save_dir}/${target}.0.tar.gz"
-	 	mv "${save_dir}/${target}.tar.gz" "${save_dir}/${target}.0.tar.gz"
-	fi
+    if [[ -f "${save_dir}/${target}.tar.gz" ]]
+    then
+        echo "Rotating: ${save_dir}/${target}.tar.gz -> ${save_dir}/${target}.0.tar.gz"
+         mv "${save_dir}/${target}.tar.gz" "${save_dir}/${target}.0.tar.gz"
+    fi
 }
 
 function compress_dir() {
 
-	if [ "$#" = 0 ]; then
-		echo $"Usage: compress_dir [target]"
-		return 1
-	fi
+    if [ "$#" = 0 ]; then
+        echo $"Usage: compress_dir [target]"
+        return 1
+    fi
 
-	local base target
+    local base target
 
-	# Strip trailing slash
-	base=${1%*/};
-	# Strip base, leaving target dir
-	target=${base##*/}
-	# Remove target dir from base path
-	base=${base%/*}
+    # Strip trailing slash
+    base=${1%*/};
+    # Strip base, leaving target dir
+    target=${base##*/}
+    # Remove target dir from base path
+    base=${base%/*}
 
-	#echo "base: $base, target: $target"
-	cd ${base}
-	echo -n "Compressing: ${base}/${target} -> ${save_dir}/${target}.tar.gz ..."
-	tar -czf "${save_dir}/${target}.tar.gz" "${base}/${target}" 2>/dev/null
-	[ $? -eq 0 ] && echo " done" || echo " failed"
+    #echo "base: $base, target: $target"
+    cd ${base}
+    echo -n "Compressing: ${base}/${target} -> ${save_dir}/${target}.tar.gz ..."
+    tar -czf "${save_dir}/${target}.tar.gz" "${base}/${target}" 2>/dev/null
+    [ $? -eq 0 ] && echo " done" || echo " failed"
 }
 
 
 for bk in "${backup[@]}"
 do
-	if [[ "$(ls ${bk} 2>/dev/null)" || -d ${bk} ]]
-	then
-		for bk_target in $bk;
-		do
-			echo "Target: \"$bk_target\"";
-			
-			rotate_archive $bk_target $keep_archive
-			compress_dir $bk_target
-		done
-	else
-		echo "Missing backup target: $bk"
-	fi
+    if [[ "$(ls ${bk} 2>/dev/null)" || -d ${bk} ]]
+    then
+        for bk_target in $bk;
+        do
+            echo "Target: \"$bk_target\"";
+            
+            rotate_archive $bk_target $keep_archive
+            compress_dir $bk_target
+        done
+    else
+        echo "Missing backup target: $bk"
+    fi
 done
